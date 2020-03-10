90ef8cd861dcddda43e4f35da860a63dafbe0920
Robert Chow <rchow@thoughtworks.com>
2020-03-10 14:56:37 +0000

    simple server push message to / on /write

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,33 @@ import express from 'express';
 const app = express();
 const port = 8080;
 
-app.get("/", (req,res) => {
-    res.send('Hello World');
+const data: string[] = [];
+
+app.get("/", (req, res) => {
+
+    res.writeHead(200, {
+        Connection: "keep-alive",
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache"
+      });
+
+      let sessionData = data.slice();
+      data.forEach((d) => res.write(`${d}\n`));
+
+      setInterval(() => {
+        const atIntervalData = data.slice();
+        if (atIntervalData.length != sessionData.length) {
+            const intervalData = atIntervalData.slice(sessionData.length);
+            intervalData.forEach((d) => res.write(`${d}\n`));
+        }
+        sessionData = atIntervalData;
+      }, 100);
+})
+.get("/write", (req, res) => {
+    data.push(`${new Date()}: ${data.length}`);
+    res.status(200).send("Added!");
 });
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}!`);
+    console.log(`Server started at http://localhost:${port}`);
 });
