340c25b65f736202ef9e8dc6a1c465606281def8
Robert Chow <rchow@thoughtworks.com>
2020-03-11 19:57:46 +0000

    Utilise websockets for push instead

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,39 @@
-import express from 'express';
+// import express from 'express';
 
-const app = express();
-const port = 8080;
+// const app = express();
+// const port = 8080;
 
-const data: string[] = [];
+// const data: string[] = [];
 
-app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
 
-    res.writeHead(200, {
-        Connection: "keep-alive",
-        "Content-Type": "text/event-stream",
-        "Cache-Control": "no-cache"
-      });
+//     res.writeHead(200, {
+//         Connection: "keep-alive",
+//         "Content-Type": "text/event-stream",
+//         "Cache-Control": "no-cache"
+//       });
 
-      let sessionData = data.slice();
-      data.forEach((d) => res.write(`${d}\n`));
+//       let sessionData = data.slice();
+//       data.forEach((d) => res.write(`${d}\n`));
 
-      setInterval(() => {
-        const atIntervalData = data.slice();
-        if (atIntervalData.length != sessionData.length) {
-            const intervalData = atIntervalData.slice(sessionData.length);
-            intervalData.forEach((d) => res.write(`${d}\n`));
-        }
-        sessionData = atIntervalData;
-      }, 100);
-})
-.get("/write", (req, res) => {
-    data.push(`${new Date()}: ${data.length}`);
-    res.status(200).send("Added!");
-});
+//       setInterval(() => {
+//         const atIntervalData = data.slice();
+//         if (atIntervalData.length != sessionData.length) {
+//             const intervalData = atIntervalData.slice(sessionData.length);
+//             intervalData.forEach((d) => res.write(`${d}\n`));
+//         }
+//         sessionData = atIntervalData;
+//       }, 100);
+// })
+// .get("/write", (req, res) => {
+//     data.push(`${new Date()}: ${data.length}`);
+//     res.status(200).send("Added!");
+// });
 
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server started at http://localhost:${port}`);
+// });
+
+import { App } from './app';
+
+new App();
