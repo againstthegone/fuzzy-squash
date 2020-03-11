619ced43b9266f1773667434b3a6d90102596488
Robert Chow <rchow@thoughtworks.com>
2020-03-11 20:29:01 +0000

    Push recieved messages to all connections via WebSockets

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,38 @@
-import { createServer } from 'http';
-import WebSocket, { Server } from 'ws';
+import { createServer, Server } from 'http';
+import WebSocket, { Server as WebSocketServer } from 'ws';
 
 class App {
-    constructor() {
-        const server = createServer();
-        const wsServer = new Server({ server });
-        
-        wsServer.on('connection', (ws: WebSocket) => {
-            console.log('connected!');
-            //connection is up, let's add a simple simple event
-            ws.on('message', (message: string) => {
-        
-                //log the received message and send it back to the client
-                console.log('received: %s', message);
-                ws.send(`Hello, you sent -> ${message}`);
-            });
-        
-            //send immediatly a feedback to the incoming connection    
-            ws.send('Hi there, I am a WebSocket server');
+    private data: string[] = [];
+
+    private onMessage = (message: string) => {
+        this.data.push(message);
+    }
+
+    private onConnection = (webSocket: WebSocket) => {
+        webSocket.on('message', this.onMessage);
+
+        let sessionData = this.data.slice();
+        sessionData.forEach((d) => {
+            webSocket.send(d);
         });
-        
-        server.listen(8081, () => {
-            console.log('Server started on at http://localhost:8081');
-        }); 
+
+        setInterval(() => {
+            const atIntervalData = this.data.slice();
+            if (atIntervalData.length != sessionData.length) {
+                const intervalData = atIntervalData.slice(sessionData.length);
+                intervalData.forEach((d) => webSocket.send(d));
+            }
+            sessionData = atIntervalData;
+        }, 100);
+    }
+
+    listen(port: number = 8080, listeningListener: () => void ): Server {
+        const server = createServer();
+        const webSocketServer = new WebSocketServer({ server });
+
+        webSocketServer.on('connection', this.onConnection);
+
+        return server.listen(port, listeningListener);
     }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,4 @@
-// import express from 'express';
-
-// const app = express();
-// const port = 8080;
-
-// const data: string[] = [];
-
-// app.get("/", (req, res) => {
-
-//     res.writeHead(200, {
-//         Connection: "keep-alive",
-//         "Content-Type": "text/event-stream",
-//         "Cache-Control": "no-cache"
-//       });
-
-//       let sessionData = data.slice();
-//       data.forEach((d) => res.write(`${d}\n`));
-
-//       setInterval(() => {
-//         const atIntervalData = data.slice();
-//         if (atIntervalData.length != sessionData.length) {
-//             const intervalData = atIntervalData.slice(sessionData.length);
-//             intervalData.forEach((d) => res.write(`${d}\n`));
-//         }
-//         sessionData = atIntervalData;
-//       }, 100);
-// })
-// .get("/write", (req, res) => {
-//     data.push(`${new Date()}: ${data.length}`);
-//     res.status(200).send("Added!");
-// });
-
-// app.listen(port, () => {
-//     console.log(`Server started at http://localhost:${port}`);
-// });
-
 import { App } from './app';
 
-new App();
+const app = new App();
+app.listen(8080, () => { console.log('WebSocket server started at ws://localhost:8080'); });
